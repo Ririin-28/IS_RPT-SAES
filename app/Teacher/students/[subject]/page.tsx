2a36fa4ff9c3e0be96f18273cebd36ab16ec77c6
eduardgomez564 <eduardgomez564@gmail.com>
2025-12-14 12:10:36 +0800

    12/14/2025 Updated params in multiple pages

--- app/Teacher/students/[subject]/page.tsx
+++ app/Teacher/students/[subject]/page.tsx
@@ -1,5 +1,10 @@
 import TeacherStudents from "@/modules/Teacher/students/students";
 
-export default function SubjectStudentsPage({ params }: { params: { subject: string } }) {
-  return <TeacherStudents subjectSlug={params.subject} />;
+export default async function SubjectStudentsPage({
+  params,
+}: {
+  params: Promise<{ subject: string }>;
+}) {
+  const { subject } = await params;
+  return <TeacherStudents subjectSlug={subject} />;
 }
