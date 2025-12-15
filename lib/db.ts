d742d46abdabf6849492100948563d0973776ff1
eduardgomez564 <eduardgomez564@gmail.com>
2025-12-15 11:36:50 +0800

    12/15/2025 Edited db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -6,7 +6,7 @@ const DB_CONFIG = {
   host: process.env.DB_HOST ?? "localhost",
   user: process.env.DB_USER ?? "root",
   password: process.env.DB_PASSWORD ?? "RIANA28@eg564",
-  database: process.env.DB_DATABASE ?? "rpt-saes_db",
+  database: process.env.DB_DATABASE ?? process.env.DB_NAME ?? "rpt-saes_db",
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   connectionLimit: process.env.DB_CONNECTION_LIMIT
     ? Number(process.env.DB_CONNECTION_LIMIT)
